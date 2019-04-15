d3bb60783dbd499ef9d132369590eda2a5863747
petavy@gmx.net <petavy@gmx.net>
2019-04-15 21:57:46 +0200

    initial commit

---
+++ main.go
@@ -0,0 +1,112 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"time"
+
+	"github.com/mpetavy/common"
+)
+
+var (
+	amount  *int
+	timeout *int
+)
+
+func init() {
+	amount = flag.Int("n", 1, "amount of parallel invocations")
+	timeout = flag.Int("t", 0, "timeout before terminate command (ms)")
+}
+
+func run() error {
+
+	if flag.NArg() == 0 {
+		fmt.Fprintf(os.Stdout, "Please provide a custom command to invoke. Sample: invoke -n 3 cmd /c echo Hello world")
+		common.Exit(0)
+	}
+
+	args := os.Args[len(os.Args)-flag.NArg():]
+
+	fmt.Print("Invoke command: ")
+	for _, c := range args {
+		fmt.Printf("\"%s\" ", c)
+	}
+	fmt.Println()
+
+	fmt.Println()
+
+	var processes []*os.Process
+
+	start := time.Now()
+
+	for i := 0; i < *amount; i++ {
+		var cmd *exec.Cmd
+
+		if len(args) > 0 {
+			cmd = exec.Command(args[0], args[1:]...)
+		} else {
+			cmd = exec.Command(args[0])
+		}
+
+		if *amount == 1 {
+			cmd.Stdin = os.Stdin
+			cmd.Stdout = os.Stdout
+			cmd.Stderr = os.Stderr
+		}
+
+		err := cmd.Start()
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Printf("Started process with PID %d\n", cmd.Process.Pid)
+
+		if err != nil {
+			if _, ok := err.(*exec.ExitError); !ok {
+				log.Fatalf("cannot run command %s: %v", args[0], err)
+			}
+		}
+
+		processes = append(processes, cmd.Process)
+	}
+
+	if *timeout > 0 {
+		d := time.Duration(*timeout) * time.Millisecond
+
+		fmt.Println()
+		fmt.Printf("Wait %v to terminate processes...\n", d)
+		fmt.Println()
+
+		time.Sleep(d)
+
+		killAll(processes)
+	} else {
+		fmt.Printf("Wait for processes to terminate...\n")
+
+		for _, process := range processes {
+			process.Wait()
+		}
+
+		fmt.Printf("\nInvoke measured time: %s\n", time.Since(start))
+	}
+
+	return nil
+}
+
+func killAll(processes []*os.Process) {
+	for _, process := range processes {
+		fmt.Printf("Kill process with PID %d\n", process.Pid)
+
+		process.Signal(os.Kill)
+	}
+}
+
+func main() {
+	defer common.Cleanup()
+
+	common.New(&common.App{"invoke", "1.0.3", "2017", "invokes a command to measure times", "mpetavy", common.APACHE, "https://github.com/mpetavy/invoke", false, nil,nil, nil, run, time.Duration(0)}, nil)
+	common.Run()
+}
